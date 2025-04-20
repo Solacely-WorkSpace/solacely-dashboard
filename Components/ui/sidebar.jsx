1773b9cd0c9d1736ed4f4a242543e990fc40762a
CHrisEl-coder <edwinchristian97@gmail.com>
2025-04-20 06:22:23 -0700

    Implement changes to shadcn imports

--- Components/ui/sidebar.jsx
+++ Components/ui/sidebar.jsx
@@ -15,7 +15,7 @@ import {
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "@/components/ui/sheet";
+} from "@/Components/ui/sheet";
 import { Skeleton } from "@/Components/ui/skeleton";
 import {
   Tooltip,
